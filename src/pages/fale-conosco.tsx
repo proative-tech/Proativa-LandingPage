33fdb0c84980833791e13fd207a7f19adf693ff8
Danrley Santos <danrleypow@gmail.com>
2021-05-03 13:05:01 -0300

    Página contato finalizada

--- src/pages/fale-conosco.tsx
+++ src/pages/fale-conosco.tsx
@@ -1,18 +1,95 @@
 import { Header } from '../components/Header';
-import { Container } from '../styles/pages/LandingPage';
+import { useMediaQuery } from 'react-responsive';
+import { Container, Content } from '../styles/pages/LandingPage';
 import { Footer } from '../components/Footer';
 import { Input } from '../components/Input';
+import { Button } from '../components/Button';
 
 export default function Index() {
+  const isMobileOrTablet = useMediaQuery({ maxWidth: 768 });
+
   return (
     <>
-    <Container>
-      <Header />
-      <div>
-        <Input label="Nome:" name="nome" placeholder="ex: Mariana Camargo"/>
-      </div>
-    </Container>
-    <Footer />
+      <Container>
+        <Header />
+        <Content>
+          <div className="content__form">
+            <h1>Deseja saber mais sobre as nossas soluções?</h1>
+            <p>Entre em contato e tire suas dúvidas:</p>
+            <form action="">
+              <div>
+                <Input
+                  label="Nome"
+                  name="nome"
+                  placeholder="ex: mariana Camargo"
+                />
+              </div>
+
+              <div className="input_line">
+                <div className="input_item">
+                  <Input
+                    label="Empresa"
+                    name="nome"
+                    placeholder="ex: Proative"
+                  />
+                </div>
+
+                <div className="input_item">
+                  <Input
+                    label="Cargo"
+                    name="nome"
+                    placeholder="ex: Coordenador"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <Input
+                  label="E-mail"
+                  name="nome"
+                  placeholder="ex: contato@proativetec.com.br"
+                />
+              </div>
+
+              <div>
+                <label>Mensagem:</label>
+                <textarea></textarea>
+              </div>
+
+              <div className="contentBtn">
+                <Button type="button">Enviar</Button>
+              </div>
+            </form>
+          </div>
+
+          {!isMobileOrTablet && (
+            <div className="content__text">
+              <strong>Atendimento:</strong>
+              <p>De segunda à sexta-feira, das 9h às 18h. +55 11 4130-8800</p>
+
+              <strong>Comercial:</strong>
+              <p>contato@proativetec.com.br</p>
+
+              <strong>Endereço: </strong>
+              <p>
+                Alameda Araguaia, 2104 – 7º andar Conj. 71A <br />
+                Alphaville | Barueri <br />
+                São Paulo, Brasil <br />
+                06455-000
+              </p>
+
+              <strong>Política de Privacidade</strong>
+              <p>
+                Os dados inseridos nessa página serão utilizados apenas para
+                solucionar sua dúvida.
+              </p>
+
+              <p>Saiba mais sobre nossa política de privacidade.</p>
+            </div>
+          )}
+        </Content>
+      </Container>
+      <Footer />
     </>
-  )
+  );
 }
--- src/styles/pages/LandingPage.ts
+++ src/styles/pages/LandingPage.ts
@@ -1,5 +1,137 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background: #f4f4f4;
+  background: #fff;
+`;
+
+export const Content = styled.div`
+  max-width: 1085px;
+  margin: 0 auto;
+  position: relative;
+  display: flex;
+  justify-content: center;
+
+  > div.content__form {
+    padding: 2rem 5rem;
+    max-width: 690px;
+    margin-left: 5rem;
+
+    h1 {
+      font-size: 2, 25rem;
+      max-width: 373px;
+      font-family: 'Source Sans Pro';
+      margin-bottom: 0.5rem;
+    }
+
+    p {
+      font-size: 1.125rem;
+      color: #6a6a6a;
+      margin-bottom: 1.5rem;
+    }
+
+    form {
+      max-width: 420px;
+      > div + div {
+        margin-bottom: 1rem;
+      }
+      .input_line {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 1rem;
+
+        .input_item {
+          width: 45%;
+        }
+      }
+
+      label {
+        font-size: 1.125rem;
+        color: #6a6a6a;
+      }
+
+      textarea {
+        width: 100%;
+        height: 156px;
+        border-radius: 15px;
+      }
+
+      .contentBtn {
+        width: 100%;
+        display: flex;
+        justify-content: flex-end;
+
+        Button {
+          width: 50%;
+        }
+      }
+    }
+  }
+
+  > div.content__text {
+    padding: 2rem 5rem;
+
+    strong {
+      font-size: 1.25rem;
+      font-family: 'Source Sans Pro';
+      font-weight: bold;
+    }
+
+    p {
+      font-family: 'Source Sans Pro';
+      font-size: 1.125rem;
+      line-height: 1.5;
+      color: #6a6a6a;
+      margin-bottom: 3rem;
+      margin-top: 0.5rem;
+    }
+
+    p:nth-child(2) {
+      max-width: 167px;
+    }
+
+    p:nth-child(6) {
+      max-width: 267px;
+    }
+
+    p:nth-child(8) {
+      max-width: 177px;
+    }
+
+    p:nth-child(9) {
+      max-width: 167px;
+      font-size: 0.875rem;
+      font-weight: 600;
+    }
+  }
+
+  @media (max-width: 860px) {
+    > div.content__form {
+      padding: 2rem 2rem;
+    }
+
+    > div.content__text {
+      padding: 2rem 1rem;
+    }
+  }
+
+  @media (max-width: 770px) {
+    flex-direction: column;
+
+    > div.content__form {
+      margin: 0 auto;
+      padding: 2rem 2rem;
+    }
+
+    > div.content__text {
+      margin: 0 auto;
+      width: 450px;
+      padding: 2rem 2rem;
+    }
+  }
+
+  @media (max-width: 430px) {
+    > div.content__form {
+      margin: 0;
+    }
+  }
 `;
