313266c62fedd3d599258c56c5cc0525cc36035b
Lucas dos Santos <lucas_rafa_94@hotmail.com>
2021-08-30 11:00:20 -0300

    ajuste favicon e video

--- src/components/LandingPart/About/index.tsx
+++ src/components/LandingPart/About/index.tsx
@@ -73,7 +73,7 @@ export const About = ({ children }: AboutProps) => {
             frameBorder="0"
             allowFullScreen
           /> */}
-          <iframe src="https://player.vimeo.com/video/593357178?h=a0cf868b9e" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+          <iframe src="https://player.vimeo.com/video/593357178?h=a0cf868b9e" width="640" height="360" allow="autoplay; fullscreen; picture-in-picture"></iframe>
         </div>
       </ContainerImg>
       <Content>
